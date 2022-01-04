28c387a739c8a24eeda7d4e26c764e7ad79286e4
tonykastaneda <tonykastaneda@gmail.com>
2022-01-04 13:10:49 -0800

    Update simpleappsfolder-cmd.bat

--- simpleappsfolder-cmd.bat
+++ simpleappsfolder-cmd.bat
@@ -1,1 +1,3 @@
-for /r "%AppData%\Microsoft\Windows\Start Menu\Programs\" %%x in (*.lnk, *.url, *.exe) do move "%%x" "%ProgramData%\Microsoft\Windows\Start Menu\Programs" && cd %ProgramData%\Microsoft\Windows\Start Menu\Programs && del *uninstall*
+call for /r "%AppData%\Microsoft\Windows\Start Menu\Programs\" %%x in (*.lnk, *.url, *.exe) do move "%%x" "%ProgramData%\Microsoft\Windows\Start Menu\Programs" 
+call cd %ProgramData%\Microsoft\Windows\Start Menu\Programs 
+call del *uninstall*
