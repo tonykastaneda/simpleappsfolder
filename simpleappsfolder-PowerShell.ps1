21e672cf2561a2f65a3463497a6e8af2ec52dacd
tonykastaneda <tonykastaneda@gmail.com>
2022-01-04 12:42:57 -0800

    Delete simpleappsfolder-PowerShell.ps1

--- simpleappsfolder-PowerShell.ps1
+++
@@ -1,3 +0,0 @@
-Move-Item -Path "%AppData%\Microsoft\Windows\Start Menu\Programs\*" -Destination "%ProgramData%\Microsoft\Windows\Start Menu\Programs" -PassThru;
-del *uninstall*;
-$o.Namespace('c:\ProgramData\Microsoft\Windows\Start Menu\Programs').Self.InvokeVerb("pintohome")
