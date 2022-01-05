0d012415a7933d76d0853c3369440d95de527aba
tonykastaneda <tonykastaneda@gmail.com>
2022-01-04 20:39:43 -0800

    Empty Folder Cleaner implementation

--- simpleappsfolder-powershell.ps1
+++ simpleappsfolder-powershell.ps1
@@ -9,8 +9,11 @@ $o = new-object -com shell.application;
 $o.Namespace('C:\ProgramData\Microsoft\Windows\Start Menu\Programs').Self.InvokeVerb("pintohome");
 
 # Removes Uninstallers from Start Menu (Does not delete programs uninstaller executable from its original install root folder)
-cd "C:\ProgramData\Microsoft\Windows\Start Menu\Programs"
-del *uninstall*
+cd "C:\ProgramData\Microsoft\Windows\Start Menu\Programs";
+del *uninstall*;
+
+# Recursivley moves programs and removes it's empty folder residue with in the newly alocated Programs folder - https://stackoverflow.com/q/38063424
+Get-ChildItem -Path ./ -Recurse -File | Move-Item -Destination ./ ; Get-ChildItem -exclude *Windows Toosl* -Path ./ -Recurse -Directory | Remove-Item;
 
 
 
